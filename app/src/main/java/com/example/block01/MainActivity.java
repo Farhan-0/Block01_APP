89e0e1ee835c1af23bea67d330fd0ba8921b4848
Farhan <disguised.stranger@gmail.com>
2022-11-04 21:59:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/block01/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.block01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonFirst,buttonSecond;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonFirst = findViewById(R.id.buttonFirst);
+        buttonSecond = findViewById(R.id.buttonSecond);
+    }
+
+    public void toDo(View v) {
+        if(v.equals(buttonFirst)){
+            v.setVisibility(View.INVISIBLE);
+        }
+        if(v.equals(buttonSecond)){
+            Toast.makeText(getApplicationContext(),"...to do to do to do...", Toast.LENGTH_LONG).show();
+        }
+    }
+}
